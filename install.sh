7a60d9c4d8fabba2f022fe5d03e87a410fbc5ab7
Chandra-sinha1999 <54039460+Chandra-sinha1999@users.noreply.github.com>
2021-02-14 23:26:08 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+sudo apt install nodejs
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install node
+
+
